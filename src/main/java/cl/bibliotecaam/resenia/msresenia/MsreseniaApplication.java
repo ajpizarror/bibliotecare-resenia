f30feff811c5b1867af7c31d36b802dffd1cd66e
VI-Alumno <VI-Alumno@duoc.acad>
2026-05-06 08:53:09 -0400

    Primer commit

---
+++ src/main/java/cl/bibliotecaam/resenia/msresenia/MsreseniaApplication.java
@@ -0,0 +1,13 @@
+package cl.bibliotecaam.resenia.msresenia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsreseniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsreseniaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/bibliotecaam/resenia/msresenia/MsreseniaApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.bibliotecaam.resenia.msresenia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsreseniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
