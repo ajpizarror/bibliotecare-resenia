84f1ff021c908a836f986908b260785b3de036eb
VI-Alumno <VI-Alumno@duoc.acad>
2026-05-06 09:21:17 -0400

    Model usuario listo

--- src/main/java/cl/bibliotecaam/resenia/msresenia/model/Usuario.java
+++ src/main/java/cl/bibliotecaam/resenia/msresenia/model/Usuario.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,9 +15,19 @@ import lombok.NoArgsConstructor;
 public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long idUsuario;
-    @Column(nullable = false, length = 30)
-    private String pnombre;
-    @Column(nullable = false, length = 30)
-    private String snombre;
+    private Long id;
+    @Column(nullable = false, length = 8)
+    private String numrunUsu;
+    @Column(nullable = false, length = 1)
+    private String dvrunUsu;
+    @Column(nullable = false,length = 30)
+    private String pnombreUsu;
+    @Column(nullable = true,length = 30)
+    private String snombreUsu;
+    @Column(nullable = false,length = 30)
+    private String appaternoUsu;
+    @Column(nullable = false,length = 30)
+    private String apmaternoUsu;
+    @Column(nullable = false)
+    private LocalDate fechaNacUsu;
 }
