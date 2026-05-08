2df586c7aa726f8a1d3334ce97e93c4db7a58de5
VI-Alumno <VI-Alumno@duoc.acad>
2026-05-08 11:43:32 -0400

    avanzando controller

---
+++ src/main/java/cl/bibliotecaam/resenia/msresenia/controller/ReseniaController.java
@@ -0,0 +1,37 @@
+package cl.bibliotecaam.resenia.msresenia.controller;
+
+import cl.bibliotecaam.resenia.msresenia.dto.ReseniaResponseDTO;
+import cl.bibliotecaam.resenia.msresenia.model.Usuario;
+import cl.bibliotecaam.resenia.msresenia.service.ReseniaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/bibliotecaam/resenias")
+@RequiredArgsConstructor
+public class ReseniaController {
+    private final ReseniaService reseniaService;
+
+    @GetMapping
+    public ResponseEntity<List<ReseniaResponseDTO>> obtenerTodas(){
+        return ResponseEntity.ok(reseniaService.listarTodas());
+    }
+
+    @GetMapping("/puntaje/{puntaje}")
+    public ResponseEntity<List<ReseniaResponseDTO>> obtenerPorPuntaje(@PathVariable Long puntaje){
+        return ResponseEntity.ok((reseniaService.listarPorPuntaje(puntaje)));
+    }
+
+    @GetMapping("/fecha/{fecha}")
+    public ResponseEntity<List<ReseniaResponseDTO>> obtenerPorFecha(@PathVariable LocalDate fecha){
+        return ResponseEntity.ok(reseniaService.listarPorFecha(fecha));
+    }
+    @GetMapping("/usuario/{usuario}")
+    public ResponseEntity<List<ReseniaResponseDTO>> obtenerPorUsuario(@RequestBody Usuario usuario){
+        return ResponseEntity.ok(reseniaService.listarPorUsuario(usuario));
+    }
+}
