9f3d1da790d0fe0e809814c68ded7126d60d928f
VI-Alumno <VI-Alumno@duoc.acad>
2026-05-13 09:45:30 -0400

    terminando validaciones

--- src/main/java/cl/bibliotecaam/resenia/msresenia/config/AppConfig.java
+++ src/main/java/cl/bibliotecaam/resenia/msresenia/config/AppConfig.java
@@ -1,4 +1,19 @@
 package cl.bibliotecaam.resenia.msresenia.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
+
 public class AppConfig {
+
+    @Value("${usuario.url}")
+    private String usuarioURL;
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .baseUrl(usuarioURL)
+                .build();
+    }
+
 }
