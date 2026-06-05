0eea5b39b8bd8a148363747f0153eee12215cac7
VI-Alumno <VI-Alumno@duoc.acad>
2026-06-05 11:53:00 -0400

    agregando swagger

---
+++ src/main/java/cl/bibliotecaam/resenia/msresenia/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package cl.bibliotecaam.resenia.msresenia.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API 2026 Listado de resenias")
+                        .version("1.0")
+                        .description("Documentacion de la API para crear el sistema de resenias"));
+    }
+}
--- src/main/java/cl/bibliotecaam/resenia/msresenia/controller/ReseniaController.java
+++ src/main/java/cl/bibliotecaam/resenia/msresenia/controller/ReseniaController.java
@@ -3,6 +3,8 @@ package cl.bibliotecaam.resenia.msresenia.controller;
 import cl.bibliotecaam.resenia.msresenia.dto.ReseniaRequestDTO;
 import cl.bibliotecaam.resenia.msresenia.dto.ReseniaResponseDTO;
 import cl.bibliotecaam.resenia.msresenia.service.ReseniaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,39 +17,47 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/bibliotecaam/resenias")
 @RequiredArgsConstructor
+@Tag(name = "Resenias", description = "Operaciones asociadas a resenias.")
 public class ReseniaController {
     private final ReseniaService reseniaService;
 
     @GetMapping
+    @Operation(summary = "Obtener todas las resenias", description = "Obtiene una lista de todas las resenias.")
     public ResponseEntity<List<ReseniaResponseDTO>> obtenerTodas(){
         return ResponseEntity.ok(reseniaService.listarTodas());
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Obtener resenias por id", description = "Obtiene una resenia acorde a un id.")
     public ResponseEntity<Optional<ReseniaResponseDTO>> obtenerPorId(@PathVariable Long id){
         return ResponseEntity.ok(reseniaService.obtenerPorId(id));
     }
 
     @GetMapping("/puntaje/{puntaje}")
+    @Operation(summary = "Obtener resenias por puntaje", description = "Obtiene una resenia acorde a un puntaje.")
     public ResponseEntity<List<ReseniaResponseDTO>> obtenerPorPuntaje(@PathVariable Long puntaje){
         return ResponseEntity.ok((reseniaService.listarPorPuntaje(puntaje)));
     }
 
     @GetMapping("/fecha/{fecha}")
+    @Operation(summary = "Obtener resenias por fecha", description = "Obtiene una resenia acorde a una fecha.")
     public ResponseEntity<List<ReseniaResponseDTO>> obtenerPorFecha(@PathVariable LocalDate fecha){
         return ResponseEntity.ok(reseniaService.listarPorFecha(fecha));
     }
     @GetMapping("/usuario/{id}")
+    @Operation(summary = "Obtener resenias por usuario", description = "Obtiene una resenia acorde a un usuario.")
     public ResponseEntity<List<ReseniaResponseDTO>> obtenerPorIdUsuario(@PathVariable Long id){
         return ResponseEntity.ok(reseniaService.listarPorUsuario(id));
     }
 
     @PostMapping
+    @Operation(summary = "Guardar una resenia", description = "Guarda una resenia acorde a lo ingresado.")
     public ResponseEntity<ReseniaResponseDTO> guardar(@Valid @RequestBody ReseniaRequestDTO doto){
         return ResponseEntity.status(201).body(reseniaService.guardar(doto));
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Actualizar resenia", description = "Actualiza una resenia acorde a una id.")
     public ResponseEntity<ReseniaResponseDTO> actualizar(@PathVariable Long id, @Valid @RequestBody ReseniaRequestDTO doto){
         return reseniaService.actualizar(id, doto)
                 .map(ResponseEntity::ok)
@@ -55,6 +65,7 @@ public class ReseniaController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Eliminar resenia", description = "Elimina una resenia acorde a una id.")
     public ResponseEntity<Void> eliminar (@PathVariable Long id){
         if (reseniaService.obtenerPorId(id).isEmpty()){
             return ResponseEntity.notFound().build();
