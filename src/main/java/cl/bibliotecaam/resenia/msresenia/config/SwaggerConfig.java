911ed15b4450b90b0e38c391d2c4d7e9e76755ee
MAGAQ <panamasado41@gmail.com>
2026-06-23 23:14:52 -0400

    Ultimos pasos para la configuracion del gateway, MS Funcionando correctamente, MS listo para el despliegue >:D

--- src/main/java/cl/bibliotecaam/resenia/msresenia/config/SwaggerConfig.java
+++ src/main/java/cl/bibliotecaam/resenia/msresenia/config/SwaggerConfig.java
@@ -1,19 +1,45 @@
 package cl.bibliotecaam.resenia.msresenia.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import java.util.List;
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${springdoc.servers[0].url:http://localhost:8098}")
+    private String serverUrl;
+
+    @Value("${springdoc.servers[0].description:Via del Api Gateway}")
+    private String serverDescription;
+
     @Bean
-    public OpenAPI customOpenAPI(){
+    public OpenAPI customOpenAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .name("bearerAuth")
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("bearerAuth");
         return new OpenAPI()
                 .info(new Info()
-                        .title("API 2026 Listado de resenias")
+                        .title("API 2026 Sistema y Registro de Reseñas de la Biblioteca AM")
                         .version("1.0")
-                        .description("Documentacion de la API para crear el sistema de resenias"));
+                        .description("Filtro y Registro de las reseñas a libros que los usuarios han hecho a traves de la Biblioteca AM"))
+                .servers(List.of(
+                        new Server()
+                                .url(serverUrl)
+                                .description(serverDescription)
+                ))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", securityScheme))
+                .addSecurityItem(securityRequirement);
     }
 }
--- src/main/java/cl/bibliotecaam/resenia/msresenia/controller/ReseniaController.java
+++ src/main/java/cl/bibliotecaam/resenia/msresenia/controller/ReseniaController.java
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -27,7 +28,7 @@ import java.util.stream.Collectors;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
-
+@SecurityRequirement(name = "bearerAuth")
 @RestController
 @RequestMapping("/api/bibliotecaam/resenia")
 @RequiredArgsConstructor
